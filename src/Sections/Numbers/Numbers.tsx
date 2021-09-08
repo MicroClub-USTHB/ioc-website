68fc075de48cdc6e710fa0fef3b3a209b95df3ff
Nidhal Anis BOUZARA <nidhalanis.bouzara@gmail.com>
2021-09-08 20:18:42 +0100

    Changed styling and content of Numbers section.

--- src/Sections/Numbers/Numbers.tsx
+++ src/Sections/Numbers/Numbers.tsx
@@ -11,19 +11,18 @@ import numbersStyle from "./Numbers.module.scss";
 // icons
 import { UilGift, UilBackpack, UilCalendarAlt, UilAward } from "@iconscout/react-unicons";
 import Card from "./Card/Card";
-import image from "../../resources/Etudz.png";
-const Sponsor = () => <img src={image} />;
 
 const Numbers = () => {
     const Lang = useSelector<RootState>((state) => state.common.Lang) as LangType;
     const language = (useSelector<RootState>((state) => state.common.language) as "english") || "french";
     return (
         <section id="Card" className={numbersStyle.section_container}>
-            <h1 className={numbersStyle.section_title + (language === "english" ? "" : " " + numbersStyle.FR)}>
-                {Lang.numbers.title}
-            </h1>
+            <div className={numbersStyle.title}>
+                <h1 className={numbersStyle.section_title + (language === "english" ? "" : " " + numbersStyle.FR)}>
+                    {Lang.numbers.title}
+                </h1>
+            </div>
             <div className={numbersStyle.cards_holder}>
-                <Card Icon={Sponsor} heading={Lang.numbers.card[4] as string} />
                 <Card Icon={UilAward} heading={Lang.numbers.card[3][0]} subHeading={Lang.numbers.card[3][1]} />
                 <Card Icon={UilGift} heading={Lang.numbers.card[0][0]} subHeading={Lang.numbers.card[0][1]} />
                 <Card Icon={UilCalendarAlt} heading={Lang.numbers.card[2] as string} />
