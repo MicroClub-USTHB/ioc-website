069ad0e785778493dfdec60f93f41c5d177329b8
Youcef Madadi <madadiyoucef@outlook.com>
2021-09-12 09:47:56 +0100

    adding heartbeat animation to user

--- src/pages/Challenges/Sections/Leaderboard/components/Leaderboard/components/Participant.tsx
+++ src/pages/Challenges/Sections/Leaderboard/components/Leaderboard/components/Participant.tsx
@@ -12,13 +12,10 @@ const Participant = (props: ParticipantProps) => {
     const { name, position, score, isUser } = props;
     return (
         <>
-            <div className={`${partStyle.container} `}>
+            <div className={partStyle.container}>
                 <span className={partStyle.position}>{position}</span>
                 <div className={partStyle.info}>
-                    <span className={partStyle.name}>
-                        {isUser && "->"}
-                        {name}
-                    </span>
+                    <span className={`${partStyle.name} ${isUser && partStyle.user}`}>{name}</span>
                     <span className={partStyle.score}>{score.toFixed(0)} Points</span>
                 </div>
             </div>
